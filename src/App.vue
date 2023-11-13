ed7ede60b0a1c70fb1722c96cc6d5bf89d29ee1a
Fantom5357 <150704141+Fantom5357@users.noreply.github.com>
2023-11-13 19:35:05 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <NavComponent></NavComponent>
+    <div>
+    <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavComponent from './components/Navigation.vue';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+export default {
+    name: 'App',
+    components: {
+      NavComponent
+    }
+
+}
+
+</script>
+
+
+<style>
+.main{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.msginput{
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-top: 3vh  ;
+  padding-bottom: 2vh;
+}
+</style>
---
+++ src/components/testApi.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="homebg text">
+        <h1>Точно первый в мире чат</h1>
+        <p>Ъ</p>
+        <div>
+            <h4>Тестовые Api(По приколу)</h4>
+            <div v-if="isLoad"></div>
+            <div v-else>
+                <div v-for="el in  teamData" :key="el.id">
+                    {{ el.id }}. {{ el.abbreviation }} {{ el.city }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TestApi',
+    data(){
+        return{
+        teamData: [],
+        isLoad: true
+        }
+    },
+    mounted(){
+        const url = 'https://free-nba.p.rapidapi.com/teams?page=0';
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'd4f9d16794msh78cc6858d7a1075p17ba17jsn37c18abeabb9',
+                'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+            }
+};
+        fetch(url, options)
+        .then((res) => res.json())
+        .then((res) => {
+            console.log(res);
+            this.teamData = res.data
+            this.isLoad = false
+        })
+}
+}
+
+</script>
